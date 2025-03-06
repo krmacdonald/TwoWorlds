3466abc1a19af4e09e7fcd39a474319d80215c87
KMacdonald <kylemacdo01@gmail.com>
2025-03-06 14:31:10 -0500

    Movement and cam

---
+++ Assets/Scripts/CamRot.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class CamRot : MonoBehaviour
+{
+    private const float YMin = -50.0f;
+    private const float YMax = 50.0f;
+
+    public Transform lookAt;
+
+    public float distance;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    public float sensivity;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        currentX += Input.GetAxis("Mouse X") * sensivity * Time.deltaTime;
+        currentY -= Input.GetAxis("Mouse Y") * sensivity * Time.deltaTime;
+        currentY = Mathf.Clamp(currentY, YMin, YMax);
+
+        Vector3 Direction = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        transform.position = lookAt.position + rotation * Direction;
+
+        transform.LookAt(lookAt.position);
+
+        lookAt.rotation = Quaternion.Euler(0f, rotation.eulerAngles.y, 0f);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    public Transform player;
+
+    private float horizontal;
+    private float vertical;
+    private Vector3 movement;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        movement = new Vector3(horizontal, 0, vertical);
+
+        player.position += player.rotation * movement * speed * Time.deltaTime;
+    }
+}
